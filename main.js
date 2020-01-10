2f495f0c26449164b5866f6cb798b6fc930d1c40
Rafa <rafa.lozano.rolon@gmail.com>
2020-01-10 09:20:39 -0600

    Funciones con JS

---
+++ main.js
@@ -0,0 +1,11 @@
+//Crear un objeto 
+var Rafa {
+    firstName: "Rafa",
+    lastName: "Lozano",
+    years: 23
+}
+
+function printData(name, years) {
+    console.log(`Bienvenido ${name} usted tiene ${years} años.`);
+}
+printData("Test", 22);
